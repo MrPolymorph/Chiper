da0c0f7e9b6bd3fe963e40da04fc6f512ec9666a
Kristopher Early <1191258+MrPolymorph@users.noreply.github.com>
2023-08-30 21:41:52 +0100

    fix errors

--- src/cpu.rs
+++ src/cpu.rs
@@ -8,16 +8,16 @@ pub struct CPU {
     pub delay_timer: u32,
     pub sound_timer: u32,
     pub registers: [u8; 16],
-    pub i: u16, 
+    pub i: usize, 
     pub pc: u16,
     pub stack_pointer: u8,
     pub instruction: u16,
     pub program_size: usize,
     pub x: usize,
-    pub y: u8,
-    pub n: u8,
-    pub nn: u8,
-    pub nnn: u16,
+    pub y: usize,
+    pub n: usize,
+    pub nn: usize,
+    pub nnn: usize,
     pub program: Vec<u8>,
 }
 
@@ -26,33 +26,31 @@ impl CPU {
     pub const DISPLAY_HEIGHT: usize = 32;
     const INSTRUCTION_START_ADDRESS: usize = 0x200;
     
-    
-    
     pub fn run(&mut self) {
         loop {
             self.fetch();
             self.execute();
         }
     }
-    
+
     /// Fetches the next instruction from memory and increments the program counter.
     fn fetch(&mut self) {
-        let hiByte = self.memory[self.pc as usize] as u16;
-        let loByte = self.memory[(self.pc + 1) as usize] as u16;
-        
-        self.instruction = (hiByte << 8) | loByte;
-        
-        self.nnn = self.instruction & 0x0FFF;
-        self.nn = (self.instruction & 0x00FF) as u8;
-        self.n = (self.instruction & 0x000F) as u8;
-        self.x = ((self.instruction & 0x0F00) >> 8) as u8;
-        self.y = ((self.instruction & 0x00F0) >> 4) as u8;
-        
+        let hi_byte = self.memory[self.pc as usize] as u16;
+        let lo_byte = self.memory[(self.pc + 1) as usize] as u16;
+
+        self.instruction = (hi_byte << 8) | lo_byte;
+
+        self.nnn = (self.instruction & 0x0FFF) as usize;
+        self.nn = (self.instruction & 0x00FF)  as usize;
+        self.n = (self.instruction & 0x000F) as usize;
+        self.x = ((self.instruction & 0x0F00) >> 8) as usize;
+        self.y = ((self.instruction & 0x00F0) >> 4) as usize;
+
         if !self.interrupt {
             self.pc += 2;
         }
     }
-    
+
     fn execute(&mut self) {
         match self.instruction {
             0x0 => return,
@@ -60,29 +58,33 @@ impl CPU {
             0xEE => {
                 self.pc = self.stack[self.stack_pointer as usize];
                 self.stack_pointer -= 1;
-            }, 
-            0x1000...0x1FFF => self.pc = self.nnn, //jump to address nn
-            0x2000...0x2FFF => self.call(), //call subroutine at address nn
-            0x3000...0x3FFF => {
-                if self.registers[self.x] == self.nn {
+            },
+            0x1000..=0x1FFF => self.pc = self.nnn as u16, //jump to address nn
+            0x2000..=0x2FFF => {
+                self.stack_pointer += 1;
+                self.stack[self.stack_pointer as usize] = self.pc;
+                self.pc = self.nnn as u16;
+            }, //call subroutine at address nn
+            0x3000..=0x3FFF => {
+                if self.registers[self.x] == self.nn as u8 {
                     self.pc += 2;
                 }
-            }, 
-            0x4000...0x4FFF => {
-                if self.registers[self.x] != self.nn {
+            },
+            0x4000..=0x4FFF => {
+                if self.registers[self.x] != self.nn as u8 {
                     self.pc += 2;
                 }
             }, //skip next instruction if vx != nn
-            05000...0x5FFF => {
+            05000..=0x5FFF => {
                 if self.registers[self.x] == self.registers[self.y] {
                     self.pc += 2;
                 }
             }, //skip next instruction if vx == vy
-            0x6000...0x6FFF => self.registers[self.x] = self.nn, //load nn into vx
-            0x7000...0x7FFF => { 
-                self.registers[self.x] += self.nn;
+            0x6000..=0x6FFF => self.registers[self.x] = self.nn as u8, //load nn into vx
+            0x7000..=0x7FFF => {
+                self.registers[self.x] += self.nn as u8;
             }, //add nn to vx
-            0x8000...0x8FFF => {
+            0x8000..=0x8FFF => {
                 match self.n {
                     0 => self.registers[self.x] = self.registers[self.y], //load vy into vx
                     1 => self.registers[self.x] |= self.registers[self.y], //vx = vx | vy
@@ -112,34 +114,38 @@ impl CPU {
                     _ => return
                 }
             }
-            0x9000...0x9FFF => {
+            0x9000..=0x9FFF => {
                 if self.registers[self.x] != self.registers[self.y] {
                     self.pc += 2;
                 }
             },
-            0xA000...0xAFFF => self.i = self.nnn,
-            0xB000...0xBFFF => self.pc = self.nnn + self.registers[0] as u16,
-            0xC000...0xCFFF => {
-                let mut rng = thread_rng();
-                let rand = rng.gen_range(0, 255);
-                self.registers[self.x] = rand;
-            },
-            0xD000...0xDFFF => self.drw(),
-            0xE000...0xEFFF => {
+            0xA000..=0xAFFF => self.i = self.nnn,
+            0xB000..=0xBFFF => self.pc = (self.nnn + self.registers[0] as usize) as u16,
+            0xC000..=0xCFFF => self.registers[self.x] = thread_rng().gen_range(0..255) as u8,
+            0xD000..=0xDFFF => self.drw(),
+            0xE000..=0xEFFF => {
                 match self.nn {
-                    0x9E => self.skp(),
-                    0xA1 => self.sknp(),
+                    0x9E => {
+                        if self.keyboard[self.registers[self.x] as usize] == 1 {
+                            self.pc += 2;
+                        }
+                    },
+                    0xA1 => {
+                        if self.keyboard[self.registers[self.x] as usize] == 0 {
+                            self.pc += 2;
+                        }
+                    },
                     _ => return
                 }
             },
-            0xF000...0xFFFF => {
+            0xF000..=0xFFFF => {
                 match self.nn {
-                    0x07 => self.ld_vx_dt(),
+                    0x07 => self.registers[self.x] = self.delay_timer as u8,
                     0x0A => self.wait(),
-                    0x15 => self.set_delay(),
-                    0x18 => self.set_sound(),
-                    0x1E => self.i_add_vx(),
-                    0x29 => self.ld_f_vx(),
+                    0x15 => self.delay_timer = self.registers[self.x] as u32,
+                    0x18 => self.sound_timer = self.registers[self.x] as u32,
+                    0x1E => self.i += self.registers[self.x] as usize,
+                    0x29 => self.i = self.registers[self.x] as usize,
                     0x33 => self.bcd(),
                     0x55 => self.dump(),
                     0x65 => self.load(),
@@ -149,7 +155,58 @@ impl CPU {
             _ => {}
         }
     }
+
+    fn wait(&mut self) {
+        for (i, key) in self.keyboard.iter().enumerate() {
+            if *key != 0 {
+                self.registers[self.x] = i as u8;
+                self.interrupt = false;
+            } else {
+                self.interrupt = true;
+            }
+        }
+
+        self.pc -= 2;
+    }
+
+    fn drw(&mut self) {
+        let x = self.registers[self.x] as usize;
+        let y = self.registers[self.y] as usize;
+        let height = self.n;
+        let mut pixel: u8;
+
+        self.registers[0xF] = 0;
+
+        for y_line in 0..height {
+            pixel = self.memory[(self.i + y_line)];
+            for x_line in 0..8 {
+                if (pixel & (0x80 >> x_line)) != 0 {
+                    if self.screen[(x + x_line + ((y + y_line) * CPU::DISPLAY_WIDTH)) as usize] == 1 {
+                        self.registers[0xF] = 1;
+                    }
+                    self.screen[(x + x_line + ((y + y_line) * CPU::DISPLAY_WIDTH)) as usize] ^= 1;
+                }
+            }
+        }
+    }
+
+    fn bcd(&mut self) {
+        self.memory[self.i as usize] = self.registers[self.x] / 100;
+        self.memory[(self.i + 1) as usize] = (self.registers[self.x] / 10) % 10;
+        self.memory[(self.i + 2) as usize] = self.registers[self.x] % 10;
+    }
     
+    fn dump(&mut self){
+        for i in 0..self.x {
+            self.memory[self.i + i] = self.registers[i];
+        }
+    }
+    
+    fn load(&mut self){
+        for i in 0..self.x {
+            self.registers[i] = self.memory[self.i + i];
+        }
+    }
     
     pub fn reset(&mut self) {
         self.registers.iter_mut().for_each(|m| *m = 0);
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ mod cpu;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let file_path = &args[1];
+    //let file_path = &args[1];
     
     //let rom = load_rom(file_path);
-    
     
     let mut cpu = cpu::CPU {
         screen: [0; cpu::CPU::DISPLAY_WIDTH * cpu::CPU::DISPLAY_HEIGHT],
@@ -36,11 +35,11 @@ fn main() {
     assert_eq!(cpu.registers[0], 45);
 }
 
-fn load_rom(file_path: &str) -> Vec<u8> {
-    let mut file = File::open(file_path).expect("Failed to open file");
-    let mut buffer = Vec::new();
-
-    file.read_to_end(&mut buffer).expect("Failed to read file");
-    
-    return buffer
-}
+// fn load_rom(file_path: &str) -> Vec<u8> {
+//     let mut file = File::open(file_path).expect("Failed to open file");
+//     let mut buffer = Vec::new();
+// 
+//     file.read_to_end(&mut buffer).expect("Failed to read file");
+//     
+//     return buffer
+// }
